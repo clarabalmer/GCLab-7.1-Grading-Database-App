a815121ed77c49ba9784a0b4448325ba6c130fb4
clara <clararbalmer@gmail.com>
2022-07-18 15:42:20 -0400

    finished lab

---
+++ GradingDatabase/src/main/java/co/grandcircus/GradingDatabase/GradingDatabaseApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.GradingDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingDatabaseApplication.class, args);
+	}
+
+}
---
+++ GradingDatabase/src/test/java/co/grandcircus/GradingDatabase/GradingDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.GradingDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
